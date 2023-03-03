afbd4e2cf78d41d9cf1ab11bb193b11bf050444a
Noah Calmette <shaikeerr@gmail.com>
2023-03-03 16:20:12 +0100

    Created JSON base

--- Slider/script.js
+++ Slider/script.js
@@ -3,6 +3,21 @@ var position=0
 var minPosition=0
 var maxPosition=3
 
+fetch('annonces.json').then(function(response) {
+    response.json().then(function(data) {              //Récupère les données du JSON et les stocke dans une variable data
+        console.log(data);
+        for (let numCase = 0; numCase < data.length; numCase++) {
+            const element = data[numCase]    //Execute la fonction analogies avec toutes les valeurs de data
+            nouveautes(element)
+        }
+    })       
+})
+
+function nouveautes(x) {
+    Slider_new = document.querySelector(".js-boxes")
+    Slider_new.innerHTML+="<div class='box'>" + x.image + "<h1 class='Prix'>" + x.prix + "</h1>" + "<img class='Rubis' src='img/Rubis.png'>" + "<button class='Bouton'>  Voir l'annonce ↦ </button>" + "<h2>" + x.nom + "</h2>" + "<p>" + x.description + "</p>" + "</div>"
+}
+
 photo = document.querySelector(".js-boxes")
 Fdroite = document.querySelector(".right")
 Fgauche = document.querySelector(".left")
